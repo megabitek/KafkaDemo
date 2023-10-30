9a30d7bb3e914e5d141dd9cd130cdb093af12598
Vladimir Bobrovskii <bobrovskiivs@gmail.com>
2023-10-30 12:26:05 +0400

    приложение демонстрирующее отправку и прием сообщений в кафку сделано по https://habr.com/ru/articles/496182/ спасибо автору..

--- src/main/java/ru/lena/bobr/kafkademo/controller/BaseController.java
+++ src/main/java/ru/lena/bobr/kafkademo/controller/BaseController.java
@@ -2,11 +2,11 @@ package ru.lena.bobr.kafkademo.controller;
 
 import dto.Address;
 import dto.UserDto;
-import org.apache.kafka.common.network.Send;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFuture;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
